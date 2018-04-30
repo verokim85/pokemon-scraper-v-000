f40c8a444b362fd7a4e4e8772b99951e5d80bdf0
Veronica Kim <vero.a.kim@gmail.com>
2018-04-30 00:30:49 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -15,12 +15,10 @@ class Pokemon
 
   def self.save(name, type, db)
     self.new(name, type, db)
-
-    pikachu_from_db VALUES (?,?,?) name, type, db)
-    # @db.execute("INSERT INTO pokemon(name, type, db)
-    # Pokemon.all.collect do |poke|
-    # Pokemon.save(poke.name, poke.type, poke.db)
-    # end
+    @db.execute("INSERT INTO pokemon(name, type, db)
+    Pokemon.all.collect do |poke|
+    Pokemon.save(poke.name, poke.type, poke.db)
+    end
   end
 
 end
