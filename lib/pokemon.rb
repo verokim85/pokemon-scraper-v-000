56da6850f210f3cf8026c86d6d8e1e36250ea6ed
Veronica Kim <vero.a.kim@gmail.com>
2018-04-30 00:23:33 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,24 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :db
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @type = type
+    @db = db
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.save(name, type, db)
+    self.new(name)
+    pikachu_from_db VALUES (?,?,?)" name, type, db)
+
+  end
+
 end
+
+
