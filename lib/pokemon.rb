6900171e42515cff4f91606927ca33c122a73cc3
Veronica Kim <vero.a.kim@gmail.com>
2018-04-30 02:24:25 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -17,6 +17,7 @@ class Pokemon
   def self.find(id, db)
     poke = db.execute("SELECT * FROM pokemon WHERE id = id").flatten
     Pokemon.new(poke[0], poke[1], poke[2], 60)
+    binding.pry
   end
 
 
