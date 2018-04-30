50ee4c666923ddb551e2cdbd63e8e574e19a94bf
Veronica Kim <vero.a.kim@gmail.com>
2018-04-30 00:39:49 +0000

    automatically backed up by learn

--- db/schema_migration.sql
+++ db/schema_migration.sql
@@ -1,1 +1,1 @@
-database_connection.execute(CREATE TABLE IF NOT EXISTS pokemon(id INTEGER PRIMARY KEY, name TEXT, type TEXT);)
+CREATE TABLE IF NOT EXISTS pokemon(id INTEGER PRIMARY KEY, name TEXT, type TEXT);
