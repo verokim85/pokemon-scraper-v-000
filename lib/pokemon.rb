be957a42451922b37810c4e979c8331d7d6f8075
Veronica Kim <vero.a.kim@gmail.com>
2018-04-30 02:00:58 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -16,8 +16,8 @@ class Pokemon
 
   def self.find(id, db)
     poke = db.execute("SELECT * FROM pokemon WHERE id = id").flatten
-    poke("ALTER TABLE pokemon ADD COLUMN hp INTEGER")
-    Pokemon.new(poke[0], poke[1], poke[2], poke[3])
+    pokes = poke("ALTER TABLE pokemon ADD COLUMN hp INTEGER")
+    Pokemon.new(pokes[0], pokes[1], pokes[2], pokes[3])
     binding.pry
   end
 
